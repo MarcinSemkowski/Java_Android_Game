eb33291ba32dc449feb4f73f72e56e57c56e3f18
Marcin <marcin96semkowski@gmail.com>
2019-11-12 20:49:31 +0100

    Method animate

--- app/src/main/java/com/example/game_connect_3/MainActivity.java
+++ app/src/main/java/com/example/game_connect_3/MainActivity.java
@@ -3,8 +3,22 @@ package com.example.game_connect_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationY(-1000f);
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate()
+                .translationYBy(1000f)
+                .setDuration(300);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
