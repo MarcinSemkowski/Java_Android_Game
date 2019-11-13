296852a7ac7e279c2d368275fca75fec35e5e8be
Marcin <marcin96semkowski@gmail.com>
2019-11-13 17:27:43 +0100

    Winning Position

--- app/src/main/java/com/example/game_connect_3/MainActivity.java
+++ app/src/main/java/com/example/game_connect_3/MainActivity.java
@@ -14,10 +14,11 @@ public class MainActivity extends AppCompatActivity {
     // 2 means un played;
 
     int[] gameState = {2,2,2,2,2,2,2,2,2};
+    int[][] winningPositions = {{0,1,2},{3,4,5} ,{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     public void dropIn(View view){
         ImageView counter = (ImageView) view;
-        System.out.println(counter.getTag().toString());
+
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
@@ -38,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
             counter.animate()
                     .translationYBy(1000f)
                     .setDuration(300);
+         for(int[] winningPosition : winningPositions){
+
+             if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                     && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                  && gameState[winningPosition[0]] != 2){
+
+                 System.out.println(gameState[winningPosition[0]]);
+             }
+         }
+
         }
 
 
