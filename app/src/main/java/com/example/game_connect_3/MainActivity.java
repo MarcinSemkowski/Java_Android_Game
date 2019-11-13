02f84a423db67e9cd1375eb1e4d5f742640cdd44
Marcin <marcin96semkowski@gmail.com>
2019-11-13 14:13:25 +0100

    Tapping Counter

--- app/src/main/java/com/example/game_connect_3/MainActivity.java
+++ app/src/main/java/com/example/game_connect_3/MainActivity.java
@@ -7,30 +7,40 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     // 0 = yellow , 1= red
 
     int activePlayer =0;
 
+    // 2 means un played;
 
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
 
     public void dropIn(View view){
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1000f);
+        System.out.println(counter.getTag().toString());
 
-        if(activePlayer ==0){
-            counter.setImageResource(R.drawable.yellow);
-          activePlayer = 1;
-        } else{
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter] == 2) {
+
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+
+            counter.animate()
+                    .translationYBy(1000f)
+                    .setDuration(300);
         }
 
 
-
-        counter.animate()
-                .translationYBy(1000f)
-                .setDuration(300);
     }
 
 
